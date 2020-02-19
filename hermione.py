24eda1f07773f56eeae9e0fc4c931f6b8eac72b4
Debjit <debjitchatterjee31@gmail.com>
2020-02-19 23:57:29 +0530

    hermione no.

--- hermione.py
+++ hermione.py
@@ -1,19 +1,21 @@
 mod = 10**9 + 7
 fact = [1]
-for i in range(1, 10^5+1):
-    fact.append(fact[i-1] * i)
+for i in range(1, 10**5+1):
+    fact.append((fact[i-1] * i)%mod)
 
 a = []
-ans = []
 n = int(input())
 a = [int(x) for x in input().split()]
 q = int(input())
+ans = [fact[a[0]]]
+for i in range(1,len(a)):
+    ans.append(fact[a[i]])
+print(ans)
 while q > 0:
     l, r = input().split()
     prod = 1
-    for i in  range(int(l), int(r)):
-        f = fact[i]
-        prod = prod * f
+    for i in  range(int(l), int(r)+1):
+        prod = (prod * ans[i])%mod
     prod = prod % mod
     h = prod ** (int(r)-int(1))
     print(h)
